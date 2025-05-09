6f6e7191bf9c8cf81ef4c8e21d96f8876e7946cc
zoiebackman <zoiebackman@icloud.com>
2025-05-09 11:57:37 +0200

    start på server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,23 @@
-async function handler() {}
+async function handler() {
+  const headers = new Headers();
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  headers.set("Access-Control-Allow-Headers", "Content-Type");
+  headers.set("Content-Type", "application/json");
+
+  if (request.method == "OPTIONS") {
+    return new Response(null, { headers: headers });
+  }
+
+  if (request.method == "GET") {
+  }
+
+  if (request.method == "POST") {
+  }
+
+  return new Response(JSON.stringify("Felaktig endpoint!"), {
+    status: 400,
+    headers: headers,
+  });
+}
 Deno.serve(handler);
