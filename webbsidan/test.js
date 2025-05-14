5f43866a690f1b00fc29f8bc81e47ad3114ba089
Moakarlssonns <mo-karlsson@hotmail.com>
2025-05-14 19:15:26 +0200

    Added new test function for creating a new user

--- webbsidan/test.js
+++ webbsidan/test.js
@@ -28,12 +28,27 @@ button.addEventListener ("click", function(){
         console.log("Test 2: logga in med existerande användare:")
         console.log( response.status);
     }
-    //Driver som returnerar array av alla användare.
+    //Driver som testar att skapa en ny användare.
+   
     async function driver_3() {
+        const newUser = { username: "Lea", password: "Häst123" }
+        const request = new Request ("http://localhost:8000/createAccount", {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(newUser)
+        })
+        const response = await fetch(request);
+        const resource = await response.json();
+        console.log("Test 3: Testar att skapa ny användare");
+        console.log(response.status);
+        console.log(resource);
+    }
+     //Driver som returnerar array av alla användare.
+     async function driver_4() {
         const request = new Request("http://localhost:8000/getUsers");
         const response = await fetch (request);
         const resource = await response.json();
-        console.log("Test 3: En array av alla användare");
+        console.log("Test 4: En array av alla användare");
         console.log(response.status);
         console.log(resource);
     }
@@ -44,6 +59,7 @@ button.addEventListener ("click", function(){
         await driver_1();
         await driver_2();
         await driver_3();
+        await driver_4();
     }
     driverHandler(); 
 })
