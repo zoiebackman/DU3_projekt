82ef335374c77a7345a5fd5960ff8c71817dd5f7
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-05-14 11:36:54 +0200

    First version of user inlog

--- server/server.js
+++ server/server.js
@@ -1,29 +1,11 @@
 async function handler() {
     const url = new URL(request.url);
 
-  const headersCORS = new Headers();
-  headersCORS.set("Access-Control-Allow-Origin", "*");
-  headersCORS.set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
-  headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
-  headersCORS.set("Content-Type", "application/json");
-
-  if (request.method == "OPTIONS") {
-    return new Response(null, {
-      status: 204,
-      headers: headersCORS,
-    });
-  }
-
-  if (contentType !== "application/json" && request.method !== "GET"){
-    return new Response (
-      JSON.stringify({ error: "Request-Body must be JSON!" }),
-      { status: 400, headers: headersCORS}
-    );
-  } 
-
-
-  if (request.method == "GET") {
-    if (url.pathname == "/login") {
+    const headersCORS = new Headers();
+    headersCORS.set("Access-Control-Allow-Origin", "*");
+    headersCORS.set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+    headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
+    headersCORS.set("Content-Type", "application/json");
 
     if (request.method == "OPTIONS") {
         return new Response(null, {
@@ -32,8 +14,42 @@ async function handler() {
         });
     }
 
+    if (contentType !== "application/json" && request.method !== "GET") {
+        return new Response(
+            JSON.stringify({ error: "Request-Body must be JSON!" }),
+            { status: 400, headers: headersCORS }
+        );
+    }
+
+
     if (request.method == "GET") {
         if (url.pathname == "/login") {
+            const userFile = "user.json"
+            const user = Deno.readTextFileSync(userFile)
+            const userArray = JSON.parse(user)
+
+            const userAccount = await request.json()
+
+            for (let user of userArray) {
+                if (user.username == userAccount.username && user.password == userAccount.password) {
+                    return new Response("Login sucess! :)", {
+                        status: 200,
+                        headers: headersCORS
+                    })
+                }
+                if (user.password != userAccount.password || user.username != userAccount.password) {
+                    return new Response("Username OR Password incorrect :(", {
+                        status: 400,
+                        headers: headersCORS
+                    })
+                }
+
+                return new Response("Account not Found! :(", {
+                    status: 409,
+                    headers: headersCORS
+                })
+            }
+
 
         }
 
@@ -60,7 +76,7 @@ async function handler() {
         if (url.pathname == "/createAccount") {
         }
 
-        i
+
     }
 }
 Deno.serve(handler);
