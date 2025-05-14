030be8d79142abebfcb9e3646cbb86d1541b3743
zoiebackman <zoiebackman@icloud.com>
2025-05-14 11:40:05 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 async function handler() {
-    const url = new URL(request.url);
+  const url = new URL(request.url);
 
-<<<<<<< HEAD
   const headersCORS = new Headers();
   headersCORS.set("Access-Control-Allow-Origin", "*");
   headersCORS.set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
@@ -15,95 +14,67 @@ async function handler() {
     });
   }
 
-  if (contentType !== "application/json" && request.method !== "GET"){
-    return new Response (
+  if (contentType !== "application/json" && request.method !== "GET") {
+    return new Response(
       JSON.stringify({ error: "Request-Body must be JSON!" }),
-      { status: 400, headers: headersCORS}
+      { status: 400, headers: headersCORS }
     );
-  } 
-
+  }
 
   if (request.method == "GET") {
     if (url.pathname == "/login") {
-    
-=======
-    const headersCORS = new Headers();
-    headersCORS.set("Access-Control-Allow-Origin", "*");
-    headersCORS.set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
-    headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
-    headersCORS.set("Content-Type", "application/json");
->>>>>>> 82ef335374c77a7345a5fd5960ff8c71817dd5f7
+      const userFile = "user.json";
+      const user = Deno.readTextFileSync(userFile);
+      const userArray = JSON.parse(user);
 
-    if (request.method == "OPTIONS") {
-        return new Response(null, {
-            status: 204,
+      const userAccount = await request.json();
+
+      for (let user of userArray) {
+        if (
+          user.username == userAccount.username &&
+          user.password == userAccount.password
+        ) {
+          return new Response("Login sucess! :)", {
+            status: 200,
             headers: headersCORS,
+          });
+        }
+        if (
+          user.password != userAccount.password ||
+          user.username != userAccount.password
+        ) {
+          return new Response("Username OR Password incorrect :(", {
+            status: 400,
+            headers: headersCORS,
+          });
+        }
+
+        return new Response("Account not Found! :(", {
+          status: 409,
+          headers: headersCORS,
         });
+      }
     }
 
-    if (contentType !== "application/json" && request.method !== "GET") {
-        return new Response(
-            JSON.stringify({ error: "Request-Body must be JSON!" }),
-            { status: 400, headers: headersCORS }
-        );
+    if (url.pathname == "/createAccount") {
     }
 
-
-    if (request.method == "GET") {
-        if (url.pathname == "/login") {
-            const userFile = "user.json"
-            const user = Deno.readTextFileSync(userFile)
-            const userArray = JSON.parse(user)
-
-            const userAccount = await request.json()
-
-            for (let user of userArray) {
-                if (user.username == userAccount.username && user.password == userAccount.password) {
-                    return new Response("Login sucess! :)", {
-                        status: 200,
-                        headers: headersCORS
-                    })
-                }
-                if (user.password != userAccount.password || user.username != userAccount.password) {
-                    return new Response("Username OR Password incorrect :(", {
-                        status: 400,
-                        headers: headersCORS
-                    })
-                }
-
-                return new Response("Account not Found! :(", {
-                    status: 409,
-                    headers: headersCORS
-                })
-            }
-
-
-        }
-
-        if (url.pathname == "/createAccount") {
-        }
-
-        if (url.pathname == "/homePage") {
-
-        }
-
-        if (url.pathname == "/homePage/Search?quiz=X") {
-
-        }
-
-        if (url.pathname == "/quizPage") {
-
-        }
+    if (url.pathname == "/homePage") {
     }
 
-    if (request.method == "POST") {
-        if (url.pathname == "/login") {
-        }
-
-        if (url.pathname == "/createAccount") {
-        }
-
-
+    if (url.pathname == "/homePage/Search?quiz=X") {
     }
+
+    if (url.pathname == "/quizPage") {
+    }
+  }
+
+  if (request.method == "POST") {
+    if (url.pathname == "/login") {
+    }
+
+    if (url.pathname == "/createAccount") {
+    }
+  }
 }
 Deno.serve(handler);
