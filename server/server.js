2b859b0fc9c115ef7864d18e871600f7fc63869b
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-05-14 11:09:28 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,48 +1,48 @@
 async function handler() {
-  const url = new URL(request.url);
+    const url = new URL(request.url);
 
-  const headersCORS = new Headers();
-  headersCORS.set("Access-Control-Allow-Origin", "*");
-  headersCORS.set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
-  headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
-  headersCORS.set("Content-Type", "application/json");
+    const headersCORS = new Headers();
+    headersCORS.set("Access-Control-Allow-Origin", "*");
+    headersCORS.set("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+    headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
+    headersCORS.set("Content-Type", "application/json");
 
-  if (request.method == "OPTIONS") {
-    return new Response(null, {
-      status: 204,
-      headers: headersCORS,
-    });
-  }
-
-  if (request.method == "GET") {
-    if (url.pathname == "/login") {
-
+    if (request.method == "OPTIONS") {
+        return new Response(null, {
+            status: 204,
+            headers: headersCORS,
+        });
     }
 
-    if (url.pathname == "/createAccount") {
+    if (request.method == "GET") {
+        if (url.pathname == "/login") {
+
+        }
+
+        if (url.pathname == "/createAccount") {
+        }
+
+        if (url.pathname == "/homePage") {
+
+        }
+
+        if (url.pathname == "/homePage/Search?quiz=X") {
+
+        }
+
+        if (url.pathname == "/quizPage") {
+
+        }
     }
 
-    if(url.pathname == "/homePage"){
+    if (request.method == "POST") {
+        if (url.pathname == "/login") {
+        }
 
+        if (url.pathname == "/createAccount") {
+        }
+
+        i
     }
-
-    if(url.pathname == "/homePage/Search?quiz=X"){
-
-    }
-
-    if(url.pathname == "/quizPage"){
-        
-    }
-  }
-
-  if (request.method == "POST") {
-    if (url.pathname == "/login") {
-    }
-
-    if (url.pathname == "/createAccount") {
-    }
-
-    i
-  }
 }
 Deno.serve(handler);
