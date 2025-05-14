fc76bcbafdce472fc9bd563d80f954b6df2069b6
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-05-14 14:04:49 +0200

    uppdaterad

--- server/server.js
+++ server/server.js
@@ -9,57 +9,49 @@ async function handler(request) {
 
     if (request.method == "OPTIONS") {
         return new Response(null, {
-            status: 204, headers: headersCORS,
-            if(request.method == "OPTIONS") {
-            return new Response(null, {
-                status: 204,
-                headers: headersCORS,
-            });
-        }
-        const contentType = request.headers.get("content-type");
+            status: 204, headers: headersCORS
+        })
+    }
 
-        if (contentType !== "application/json" && request.method !== "GET") {
-            return new Response(
-                JSON.stringify({ error: "Request-Body must be JSON!" }),
-                { status: 400, headers: headersCORS }
-            );
-        }
+    const contentType = request.headers.get("content-type");
 
-        if (request.method == "GET") {
-        }
+    if (contentType !== "application/json" && request.method !== "GET") {
+        return new Response(
+            JSON.stringify({ error: "Request-Body must be JSON!" }),
+            { status: 400, headers: headersCORS }
+        );
+    }
 
-        if (request.method == "POST") {
-            if (url.pathname == "/login") {
-                const userFile = "user.json";
-                const user = Deno.readTextFileSync(userFile);
-                const userArray = JSON.parse(user);
+    if (request.method == "POST") {
+        if (url.pathname == "/login") {
+            const userFile = "user.json";
+            const user = Deno.readTextFileSync(userFile);
+            const userArray = JSON.parse(user);
 
-                const userAccount = await request.json();
-                let userFound = false;
+            const userAccount = await request.json();
+            let userFound = false;
 
-                for (let user of userArray) {
-                    if (
-                        user.username == userAccount.username &&
-                        user.password == userAccount.password
-                    ) {
-                        userFound = true;
-                        break;
-                    }
-                }
-                if (userFound) {
-                    return new Response(JSON.stringify({ message: "Login successful!" }), {
-                        status: 200,
-                        headers: headersCORS,
-                    });
-                } else {
-                    return new Response(
-                        JSON.stringify({ error: "Wrong username OR password" }),
-                        { status: 400, headers: headersCORS }
-                    );
+            for (let user of userArray) {
+                if (
+                    user.username == userAccount.username &&
+                    user.password == userAccount.password
+                ) {
+                    userFound = true;
+                    break;
                 }
             }
+            if (userFound) {
+                return new Response(JSON.stringify({ message: "Login successful!" }), {
+                    status: 200,
+                    headers: headersCORS,
+                });
+            } else {
+                return new Response(
+                    JSON.stringify({ error: "Wrong username OR password" }),
+                    { status: 400, headers: headersCORS }
+                );
+            }
         }
-
         if (url.pathname == "/createAccount") {
             const userFile = "user.json";
             const user = Deno.readTextFileSync(userFile);
@@ -89,82 +81,12 @@ async function handler(request) {
                         headers: headersCORS,
                     });
                 }
-
-                const contentType = request.headers.get("content-type");
-
-                if (contentType !== "application/json" && request.method !== "GET") {
-                    return new Response(
-                        JSON.stringify({ error: "Request-Body must be JSON!" }),
-                        { status: 400, headers: headersCORS }
-                    );
-                }
-
-                if (request.method == "GET") {
-                }
-
-                if (request.method == "POST") {
-                    if (url.pathname == "/login") {
-                        const userFile = "user.json";
-                        const user = Deno.readTextFileSync(userFile);
-                        const userArray = JSON.parse(user);
-
-                        const userAccount = await request.json();
-                        let userFound = false;
-
-                        for (let user of userArray) {
-                            if (
-                                user.username == userAccount.username &&
-                                user.password == userAccount.password
-                            )
-                                userFound = true;
-                            break;
-                        }
-                    }
-                    if (userFound) {
-                        return new Response(JSON.stringify({ message: "Login successful!" }),
-                            { status: 200, headers: headersCORS }
-                        );
-                    } else {
-                        return new Response(
-                            JSON.stringify({ error: "Wrong username OR password" }),
-                            { status: 400, headers: headersCORS }
-                        );
-                    }
-                }
-
-                if (url.pathname == "/createAccount") {
-                    const userFile = "user.json";
-                    const user = Deno.readTextFileSync(userFile);
-                    const userArray = JSON.parse(user);
-
-                    const newUserAccount = request.json();
-
-                    for (let user of userArray) {
-                        if (user.username == newUserAccount.username) {
-                            return new Response(
-                                JSON.stringify(
-                                    { error: "Username already exist" },
-                                    { status: 409, headers: headersCORS }
-                                )
-                            );
-                        }
-
-                        if (user.username != newUserAccount.username) {
-                            newUserAccount.score = 0;
-                            userArray.push(newUserAccount);
-                            Deno.writeTextFileSync(userFile, JSON.stringify(userArray))
-
-                            return new Response(JSON.stringify({ message: "account added!" }), {
-                                status: 200,
-                                headers: headersCORS
-                            })
-                        }
-                    }
-                }
-
-                return new Response(
-                    JSON.stringify({ error: "Not Found" }),
-                    { status: 404, headers: headersCORS });
             }
+        }
+    }
+    return new Response(
+        JSON.stringify({ error: "Not Found" }),
+        { status: 404, headers: headersCORS });
+}
 
-            Deno.serve(handler);
+Deno.serve(handler);
