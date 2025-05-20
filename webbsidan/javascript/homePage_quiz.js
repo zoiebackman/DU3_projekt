201eea92a7aa6e6ef6dbcdb98cf9afdf34ec3035
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-05-20 15:32:08 +0200

    Debug, quizsidan

--- webbsidan/javascript/homePage_quiz.js
+++ webbsidan/javascript/homePage_quiz.js
@@ -5,19 +5,36 @@
 
 const quizButton = document.getElementById("geographyQuiz")
 const quizContainer = document.getElementById("mainContainer")
+const quizButtons = document.querySelectorAll(".quizButton")
+let quizData;
+//fixa så att geography ändras beroende på vilken knapp
+let quizCategory;
+/* console.log(quizContainer)
+console.log("yomnán")
+quizContainer.addEventListener("click", function () {
+    quizCategory = quizButtons.textContent
+    console.log(quizCategory)
+}) */
+
+for (let quiz of quizContainer.children) {
+    quiz.addEventListener("click", function () {
+        quizCategory = quiz.textContent
+    })
+}
 
 quizButton.addEventListener("click", function () {
     getQuiz("geography")
     quizContainer.innerHTML = ""
     getImage("geography")
-
 })
+
+// export { quizData }
 
 async function getQuiz(quizCategory) {
     const request = `https://the-trivia-api.com/api/questions?categories=${quizCategory}&limit=8&region=SE&difficulty=easy`
     const response = await fetch(request)
     console.log(response)
-    const quizData = await response.json()
+    quizData = await response.json()
     console.log(quizData)
 
     localStorage.setItem(`category${quizCategory}`, JSON.stringify(quizData)) // spara data i lokal fil via webbläsaren 
@@ -26,6 +43,9 @@ async function getQuiz(quizCategory) {
     //spara ner fil lokalt?
 
 }
+// await getQuiz(export { quizData })
+
+// const data = getQuiz(quizCategory);
 
 async function getImage(quizCategory) {
     const request = `https://api.pexels.com/v1/search?query=${quizCategory}&per_page=9`
@@ -45,8 +65,14 @@ async function getImage(quizCategory) {
     startQuizButton.id = "startQuizButton"
     quizContainer.appendChild(startQuizButton)
 
+    startQuizButton.addEventListener("click", function () {
+        window.location.href = "quizPage.html"
+    })
 }
+
+
 
 // getQuiz()
 
+// hämta alla användare i ordning på score
 
---
+++ webbsidan/javascript/quizPage.js
@@ -0,0 +1,8 @@
+// import * as homePage from homePage_quiz.js
+
+
+import { quizData } from './homePage_quiz.js'
+
+console.log(quizData)
+
+// homePage.getQuiz()
