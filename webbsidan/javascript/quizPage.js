854e13e3f101112fbeb25c0f95306abebda9848c
zoiebackman <zoiebackman@icloud.com>
2025-05-22 09:21:28 +0200

    quizPage

--- webbsidan/javascript/quizPage.js
+++ webbsidan/javascript/quizPage.js
@@ -6,49 +6,97 @@ async function getQuiz(quizCategory) {
   const request = `https://the-trivia-api.com/api/questions?categories=${quizCategory}&limit=8&region=SE&difficulty=easy`;
   const response = await fetch(request);
   const quizData = await response.json();
-  // console.log(quizData);
-
-  /* localStorage.setItem(`category${quizCategory}`, JSON.stringify(quizData)); // spara data i lokal fil via webbläsaren
-  const quiz = JSON.parse(localStorage.getItem(`category${quizCategory}`)); //hämta lokala filen
-  console.log(quiz); */
 
   let counter = 0;
-  let answerCounter = 0;
+  let scoreCounter = 0;
 
   question1.textContent = `Question ${counter + 1} : ${
     quizData[counter].question
   }`;
-  countDown.addEventListener("click", function nextQuestion() {
-    counter++;
+  console.log(quizData);
+  const newArray = [
+    { text: quizData[counter].correctAnswer, isCorrect: true },
+    { text: quizData[counter].incorrectAnswers[0], isCorrect: false },
+    { text: quizData[counter].incorrectAnswers[1], isCorrect: false },
+    { text: quizData[counter].incorrectAnswers[2], isCorrect: false },
+  ];
 
+  newArray.sort(() => Math.random() - 0.5);
+
+  answers.forEach((button, i) => {
+    button.textContent = newArray[i].text;
+  });
+
+  answers.forEach((button, i) => {
+    button.addEventListener("click", function () {
+      if (newArray[i].isCorrect === true) {
+        button.style.backgroundColor = "green";
+        counter++;
+        scoreCounter++;
+        setTimeout(() => {
+          counter++;
+          nextQuestion();
+        }, 1000);
+      }
+      if (newArray[i].isCorrect === false) {
+        button.style.backgroundColor = "red";
+        counter++;
+        setTimeout(() => {
+          counter++;
+          nextQuestion();
+        }, 1000);
+      }
+    });
+  });
+
+  function nextQuestion() {
     if (counter < quizData.length) {
+      answers.forEach((button) => {
+        button.style.backgroundColor = "";
+      });
+
       question1.textContent = `Question ${counter + 1} : ${
         quizData[counter].question
       }`;
+
       const newArray = [
-        { text: quizData[answerCounter].correctAnswer, isCorrect: true },
-        { text: quizData[answerCounter].incorrectAnswers[0], isCorrect: false },
-        { text: quizData[answerCounter].incorrectAnswers[1], isCorrect: false },
-        { text: quizData[answerCounter].incorrectAnswers[2], isCorrect: false },
+        { text: quizData[counter].correctAnswer, isCorrect: true },
+        { text: quizData[counter].incorrectAnswers[0], isCorrect: false },
+        { text: quizData[counter].incorrectAnswers[1], isCorrect: false },
+        { text: quizData[counter].incorrectAnswers[2], isCorrect: false },
       ];
-      answerCounter++;
+
+      newArray.sort(() => Math.random() - 0.5);
+
       answers.forEach((button, i) => {
         button.textContent = newArray[i].text;
       });
 
-      answers.forEach((button) => {
+      answers.forEach((button, i) => {
         button.addEventListener("click", function () {
-          if (newArray.isCorrect){
+          if (newArray[i].isCorrect === true) {
             button.style.backgroundColor = "green";
+            scoreCounter++;
+            setTimeout(() => {
+              counter++;
+            }, 1000);
+          }
+          if (newArray[i].isCorrect === false) {
+            button.style.backgroundColor = "red";
+            setTimeout(() => {
+              counter++;
+            }, 1000);
           }
         });
       });
     } else {
       question1.textContent = "Quizet är slut!";
+      answers.forEach((button, i) => {
+        button.textContent = "";
+        button.style.backgroundColor = "#5bb0ac00";
+      });
     }
-  });
-
-  //spara ner fil lokalt?
+  }
 }
 
 getQuiz("science");
