75e2117ede09d02d6ff8428a737c415423af7166
zoiebackman <zoiebackman@icloud.com>
2025-05-23 09:40:19 +0200

    fix tid i quiz, samt topplayers

--- webbsidan/javascript/quizPage.js
+++ webbsidan/javascript/quizPage.js
@@ -10,48 +10,27 @@ async function getQuiz(quizCategory) {
 
   let counter = 0;
   let scoreCounter = 0;
-  /* 
-  question1.textContent = `Question ${counter + 1} : ${
-    quizData[counter].question
-  }`;
-  console.log(quizData);
-  const newArray = [
-    { text: quizData[counter].correctAnswer, isCorrect: true },
-    { text: quizData[counter].incorrectAnswers[0], isCorrect: false },
-    { text: quizData[counter].incorrectAnswers[1], isCorrect: false },
-    { text: quizData[counter].incorrectAnswers[2], isCorrect: false },
-  ];
 
-  newArray.sort(() => Math.random() - 0.5);
-
-  answers.forEach((button, i) => {
-    button.textContent = newArray[i].text;
-  });
-
-  answers.forEach((button, i) => {
-    button.addEventListener("click", function () {
-      if (newArray[i].isCorrect === true) {
-        button.style.backgroundColor = "green";
-
-        scoreCounter++;
-        setTimeout(() => {
-          counter++;
-          nextQuestion();
-        }, 500);
-      }
-      if (newArray[i].isCorrect === false) {
-        button.style.backgroundColor = "red";
-
-        setTimeout(() => {
-          counter++;
-          nextQuestion();
-        }, 500);
-      }
-    });
-  }); */
   nextQuestion();
 
   function nextQuestion() {
+    let seconds = 21;
+    countDown.textContent = 21;
+    function countDownSeconds() {
+      if (countDown.textContent == seconds) {
+        seconds--;
+        countDown.textContent = String(seconds);
+        setTimeout(() => {
+          countDownSeconds();
+        }, 1000);
+      }
+      if (countDown.textContent <= 0) {
+        counter++;
+        nextQuestion();
+      }
+    }
+    countDownSeconds();
+
     answersBox.innerHTML = `
     <div class="answerFormat" id="answer1"></div>
     <div class="answerFormat" id="answer2"></div>
@@ -59,6 +38,7 @@ async function getQuiz(quizCategory) {
     <div class="answerFormat" id="answer4"></div>
     `;
     const answers = document.querySelectorAll(".answerFormat");
+
     if (counter < quizData.length) {
       answers.forEach((button) => {
         button.style.backgroundColor = "";
@@ -102,8 +82,24 @@ async function getQuiz(quizCategory) {
         });
       });
     } else {
-      question1.textContent = "Quizet är slut!";
-      imageContainer.textContent = `Du fick ${scoreCounter} rätt av 8 möjliga`;
+      const button = document.createElement("button");
+      button.classList.add("endbutton");
+      button.textContent = "Back to start";
+      question1.textContent = "Quiz is done!";
+      const finalText = document.createElement("div");
+      finalText.textContent = `You scored ${scoreCounter} out of 8`;
+      finalText.classList.add("finalText");
+      imageContainer.style.display = "flex";
+      imageContainer.style.flexDirection = "column";
+      imageContainer.style.justifyContent = "center";
+      imageContainer.style.alignItems = "center";
+      imageContainer.appendChild(finalText);
+      imageContainer.appendChild(button);
+
+      button.addEventListener("click", function () {
+        window.location.href = "HomePage.html";
+      });
+
       answers.forEach((button) => {
         button.textContent = "";
         button.style.backgroundColor = "#5bb0ac00";
