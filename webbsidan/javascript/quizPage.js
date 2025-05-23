980dc783e158b9ae87b77cdfddab2ea9b6f5b537
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-05-23 12:21:36 +0200

    Update quizPage.js

--- webbsidan/javascript/quizPage.js
+++ webbsidan/javascript/quizPage.js
@@ -26,128 +26,127 @@ async function getQuiz(quizCategory) {
   let scoreCounter = 0;
 
   async function getImages(quizCategory) {
-/*   async function getImage(quizCategory) {
-  const request = `https://api.pexels.com/v1/search?query=${quizCategory}&per_page=9`;
-  const options = {
-    headers: {
-      Authorization:
-        "V3C5EBsKEQBS1WAmameHcgifua6v5QP6tOmDbzBVmOSPGs0TIgGzENsT",
-    },
-  };
-  const response = await fetch(request, options);
-  images = await response.json();
-  console.log(images);
-}
-console.log(images)
-
-await getImages(quizCategory);   /// vänta in objektet med bilder, anropa sedan
-
-function questionImages(indexOfImage) {
-  imageContainer.innerHTML = `<img src=${images.photos[indexOfImage].src.medium} width="500" height="300" style="object-fit:contain;">`;
-  imageContainer.style.display = "flex";
-  imageContainer.style.justifyContent = "center";
-  imageContainer.style.alignItems = "center";
-  console.log(images)
-}
-
-nextQuestion();
-
-function nextQuestion() {
-  let seconds = 21;
-  countDown.textContent = 21;
-  function countDownSeconds() {
-    if (countDown.textContent == seconds) {
-      seconds--;
-      countDown.textContent = String(seconds);
-      setTimeout(() => {
-        countDownSeconds();
-      }, 1000);
+    async function getImage(quizCategory) {
+      const request = `https://api.pexels.com/v1/search?query=${quizCategory}&per_page=9`;
+      const options = {
+        headers: {
+          Authorization:
+            "V3C5EBsKEQBS1WAmameHcgifua6v5QP6tOmDbzBVmOSPGs0TIgGzENsT",
+        },
+      };
+      const response = await fetch(request, options);
+      images = await response.json();
+      console.log(images);
     }
-    if (countDown.textContent <= 0) {
-      counter++;
-      nextQuestion();
+    console.log(images)
 
+    await getImages(quizCategory);   /// vänta in objektet med bilder, anropa sedan
+
+    function questionImages(indexOfImage) {
+      imageContainer.innerHTML = `<img src=${images.photos[indexOfImage].src.medium} width="500" height="300" style="object-fit:contain;">`;
+      imageContainer.style.display = "flex";
+      imageContainer.style.justifyContent = "center";
+      imageContainer.style.alignItems = "center";
+      console.log(images)
     }
-  }
-  countDownSeconds();
 
-  answersBox.innerHTML = `
+    nextQuestion();
+
+    function nextQuestion() {
+      let seconds = 21;
+      countDown.textContent = 21;
+      function countDownSeconds() {
+        if (countDown.textContent == seconds) {
+          seconds--;
+          countDown.textContent = String(seconds);
+          setTimeout(() => {
+            countDownSeconds();
+          }, 1000);
+        }
+        if (countDown.textContent <= 0) {
+          counter++;
+          nextQuestion();
+
+        }
+      }
+      countDownSeconds();
+
+      answersBox.innerHTML = `
   <div class="answerFormat" id="answer1"></div>
   <div class="answerFormat" id="answer2"></div>
   <div class="answerFormat" id="answer3"></div>
   <div class="answerFormat" id="answer4"></div>
   `;
-  const answers = document.querySelectorAll(".answerFormat");
+      const answers = document.querySelectorAll(".answerFormat");
 
-  if (counter < quizData.length) {
+      if (counter < quizData.length) {
 
-    questionImages(counter) //skickar med countern till funktionen för att ta ut bild av index
+        questionImages(counter) //skickar med countern till funktionen för att ta ut bild av index
 
-    question1.textContent = `Question ${counter + 1} : ${quizData[counter].question
-      }`;
-    const newArray = [ //döpa om?
-      { text: quizData[counter].correctAnswer, isCorrect: true },
-      { text: quizData[counter].incorrectAnswers[0], isCorrect: false },
-      { text: quizData[counter].incorrectAnswers[1], isCorrect: false },
-      { text: quizData[counter].incorrectAnswers[2], isCorrect: false },
-    ];
+        question1.textContent = `Question ${counter + 1} : ${quizData[counter].question
+          }`;
+        const newArray = [ //döpa om?
+          { text: quizData[counter].correctAnswer, isCorrect: true },
+          { text: quizData[counter].incorrectAnswers[0], isCorrect: false },
+          { text: quizData[counter].incorrectAnswers[1], isCorrect: false },
+          { text: quizData[counter].incorrectAnswers[2], isCorrect: false },
+        ];
 
-    newArray.sort(() => Math.random() - 0.5);
+        newArray.sort(() => Math.random() - 0.5);
 
-    answers.forEach((button, i) => {
-      button.textContent = newArray[i].text;
-    });
+        answers.forEach((button, i) => {
+          button.textContent = newArray[i].text;
+        });
 
-    answers.forEach((button, i) => {
-      button.addEventListener("click", function () {
-        counter++;
-        if (newArray[i].isCorrect === true) {
-          button.style.backgroundColor = "green";
-          scoreCounter++;
-          setTimeout(() => {
-            //counter++;
-            nextQuestion();
-          }, 500);
-        }
-        if (newArray[i].isCorrect === false) {
-          button.style.backgroundColor = "red";
-          setTimeout(() => {
-            //counter++;
-            nextQuestion();
-          }, 500);
-        }
-      });
+        answers.forEach((button, i) => {
+          button.addEventListener("click", function () {
+            counter++;
+            if (newArray[i].isCorrect === true) {
+              button.style.backgroundColor = "green";
+              scoreCounter++;
+              setTimeout(() => {
+                //counter++;
+                nextQuestion();
+              }, 500);
+            }
+            if (newArray[i].isCorrect === false) {
+              button.style.backgroundColor = "red";
+              setTimeout(() => {
+                //counter++;
+                nextQuestion();
+              }, 500);
+            }
+          });
 
-    });
-  } else {
-    imageContainer.innerHTML = "" //ta bort bilden till sista scoreSidan
-    const button = document.createElement("button");
-    button.classList.add("endbutton");
-    button.textContent = "Back to start";
-    question1.textContent = "Quiz is done!";
-    const finalText = document.createElement("div");
-    finalText.textContent = `You scored ${scoreCounter} out of 8`;
-    finalText.classList.add("finalText");
-    imageContainer.style.flexDirection = "column";
-    imageContainer.style.display = "flex";
-    imageContainer.style.justifyContent = "center";
-    imageContainer.style.alignItems = "center";
-    imageContainer.appendChild(finalText);
-    imageContainer.appendChild(button);
+        });
+      } else {
+        imageContainer.innerHTML = "" //ta bort bilden till sista scoreSidan
+        const button = document.createElement("button");
+        button.classList.add("endbutton");
+        button.textContent = "Back to start";
+        question1.textContent = "Quiz is done!";
+        const finalText = document.createElement("div");
+        finalText.textContent = `You scored ${scoreCounter} out of 8`;
+        finalText.classList.add("finalText");
+        imageContainer.style.flexDirection = "column";
+        imageContainer.style.display = "flex";
+        imageContainer.style.justifyContent = "center";
+        imageContainer.style.alignItems = "center";
+        imageContainer.appendChild(finalText);
+        imageContainer.appendChild(button);
 
-    button.addEventListener("click", function () {
-      window.location.href = "HomePage.html";
-    });
+        button.addEventListener("click", function () {
+          window.location.href = "HomePage.html";
+        });
 
-    answers.forEach((button) => {
-      button.textContent = "";
-      button.style.backgroundColor = "#5bb0ac00";
-    });
+        answers.forEach((button) => {
+          button.textContent = "";
+          button.style.backgroundColor = "#5bb0ac00";
+        });
+      }
+    }
   }
-}
 }
 
 getQuiz("science");
 
-//importera variabel från homePage, vilken kategori på quiz som ska användas som argument i qetQuiz. fråga sebbe
-//läckt API? fråga GitGuardian
